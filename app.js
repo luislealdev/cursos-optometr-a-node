8e04bc3766af51b92e8c17a82c30b317633cf20a
Luis Leal <luisrrleal@gmail.com>
2022-11-03 17:45:36 -0600

    First commit

--- app.js
+++ app.js
@@ -154,6 +154,7 @@ app.post("/registro", (req, res) => {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Server is on");
-});
+app.listen(process.env.PORT || 3000),
+  function () {
+    console.log("Server is running on port 3000");
+  };
